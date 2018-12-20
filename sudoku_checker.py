5c37490d11cb140920f6f01c4e149e3dc105c277
Abdallah Aboelela <abdallahaboelela@Abdallahs-MacBook-Pro.local>
2018-12-20 20:36:15 +0200

    starting project sudoku method 1

--- sudoku_checker.py
+++ sudoku_checker.py
@@ -11,9 +11,9 @@ DONE 2. check if row completed (row_complete)
 DONE 3. check if column completed
 DONE 4. check if box completed
 DONE 5. check if board completed (boolean), using 2, 3, 4
-6. given square (coordinates), function to check what values are possible based on position 
-  if it's empty (maybe use a separate dictionary?)
-7. helper that assigns all empty squares the list of possible values using 6
+DONE 6. given square (coordinates), function to check what values are possible based on position 
+        if it's empty (maybe use a separate dictionary?)
+7. helper that assigns all empty squares the set of possible values using 6
 8. Take a grid, a position, and an assigned number, 
   and removes the number from possible options for all other empty slots it conflicts with
 9. Recursive function that takes prepared grid (see 7):
@@ -41,7 +41,7 @@ def row_complete(board, num_row):
     return False
   else:
     for num in range(9):
-      if duplicate_checker.get(num, False):
+      if duplicate_checker.get(num, False) or type(num) == set:
         return False
       else:
         duplicate_checker[num] = True
@@ -52,7 +52,7 @@ def col_complete(board, num_col):
   col = {}
   for row in board:
     num = row[num_col]
-    if num == 0 or col.get(num, False):
+    if num == 0 or col.get(num, False) or type(num) == set:
       return False
     else:
       col[num] = True
@@ -85,7 +85,7 @@ def box_complete(board, position):
   for i in range(x-1, x+2):
     for j in range(y-1, y+2):
       num = board[i][j]
-      if num == 0 or duplicate_checker.get(num, False):
+      if num == 0 or duplicate_checker.get(num, False) or type(num) == set:
         return False
       else:
         duplicate_checker[num] = True
@@ -111,21 +111,36 @@ def board_complete(board):
     else:
       return False
   return True
-    
-
   
+def possible_values_at_position(board, position):
+  assert(board[position[0]][position[1]] == 0)
+  
+  not_possible_values = board[position[0]]
+  
+  col = []
+  for i in range(9):
+    col.append(board[i][position[1]])
+  not_possible_values += col
+    
+  box = []
+  x, y = box_helper(position)
+  for i in range(x-1, x+2):
+    for j in range(y-1, y+2):
+      box.append(board[i][j])
+  not_possible_values += box
+  
+  not_possible_values = set(not_possible_values + [0]) 
+  return set(range(1, 10)) - not_possible_values
+
+def starter(board):
+  for i in range(9):
+    for j in range(9):
+      if board[i][j] == 0:
+        board[i][j] = possible_values_at_position(board, (i, j))
+  return board # necessary?
+
+incomplete_board = [[8, 7, 6, 0, 1, 0, 3, 9, 2],[0, 2, 9, 0, 7, 0, 0, 5, 0],[4, 0, 5, 2, 9, 0, 7, 0, 8],[7, 4, 1, 5, 6, 0, 0, 0, 3],[5, 0, 2, 0, 0, 4, 0, 6, 1],[0, 0, 3, 1, 0, 0, 0, 0, 0],[0, 1, 0, 0, 0, 0, 2, 4, 0],[0, 5, 4, 0, 0, 8, 0, 3, 7],[2, 9, 0, 0, 4, 1, 6, 8, 0]]
 ''' FOR DEBUGGING PURPOSES
-
-incomplete board = [[8, 7, 6, 0, 1, 0, 3, 9, 2], 
-[0, 2, 9, 0, 7, 0, 0, 5, 0], 
-[4, 0, 5, 2, 9, 0, 7, 0, 8], 
-[7, 4, 1, 5, 6, 0, 0, 0, 3], 
-[5, 0, 2, 0, 0, 4, 0, 6, 1], 
-[0, 0, 3, 1, 0, 0, 0, 0, 0],
-[0, 1, 0, 0, 0, 0, 2, 4, 0],
-[0, 5, 4, 0, 0, 8, 0, 3, 7],
-[2, 9, 0, 0, 4, 1, 6, 8, 0]]
-
 complete_wo_boxes = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
 [2, 3, 4, 5, 6, 7, 8, 9, 1],
 [3, 4, 5, 6, 7, 8, 9, 1, 2],
@@ -138,12 +153,4 @@ complete_wo_boxes = [[1, 2, 3, 4, 5, 6, 7, 8, 9],
   
 true_box = [[1, 3, 5],[2, 7, 9],[8, 6, 4]]
 '''
-
-
-
-
-
-
-
-  
   
